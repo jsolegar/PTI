2aa0292b226c4379dc0c08b142ce933b85fb0d42
Jordi <jordi.sole.garcia@estudiantat.upc.edu>
2023-02-19 20:10:02 +0100

    escritura

--- servlets/apache-tomcat-10.0.10/webapps/my_webapp/WEB-INF/classes/mypackage/CarRentalList.java
+++ servlets/apache-tomcat-10.0.10/webapps/my_webapp/WEB-INF/classes/mypackage/CarRentalList.java
@@ -4,6 +4,16 @@ import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+
 public class CarRentalList extends HttpServlet {
 
   int cont = 0;
@@ -14,10 +24,28 @@ public class CarRentalList extends HttpServlet {
     PrintWriter out = res.getWriter();
     String nombre = req.getParameter("userid");
     String password = req.getParameter("password");
-    if (nombre != null && nombre.equals("jordi") && password != null && password.equals("1234")) { //Imprimir JSON
-		    out.println("<html>" + 
-		    	"<h1>Hello World!</h1>" +
-		    "</html>");
+    if (nombre != null && nombre.equals("admin") && password != null && password.equals("1234")) { //Imprimir JSON
+		 JSONParser parser = new JSONParser();
+
+        try (Reader reader = new FileReader("orders.json")) {
+	out.println("HOLA");
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+           // out.println(jsonObject);
+
+           
+            // loop array
+            JSONArray orders = (JSONArray) jsonObject.get("list");
+            Iterator<String> iterator = orders.iterator();
+            while (iterator.hasNext()) {
+               String co2 = (String) jsonObject.get("CO2_rating");
+               out.println("<html><big>CO2 rating:"+ co2 +"</big><br></html>");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
     else {	
 	    out.println("<html>" + 
--- servlets/apache-tomcat-10.0.10/webapps/my_webapp/WEB-INF/classes/mypackage/CarRentalNew.java
+++ servlets/apache-tomcat-10.0.10/webapps/my_webapp/WEB-INF/classes/mypackage/CarRentalNew.java
@@ -4,18 +4,52 @@ import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CarRentalNew extends HttpServlet {
 
   int cont = 0;
-
+  JSONArray list = new JSONArray();
   public void doGet(HttpServletRequest req, HttpServletResponse res)
                     throws ServletException, IOException {
     res.setContentType("text/html");
     PrintWriter out = res.getWriter();
-    String nombre = req.getParameter("name");
-    cont ++;
-    out.println("<html><big>Hola Amigo "+ nombre + "</big><br>"+
-                cont + " Accesos desde su carga.</html>");
+    String co2 = req.getParameter("co2_rating");
+    String engine = req.getParameter("sub_model_vehicle");
+    String rentDays = req.getParameter("dies_lloguer");
+    String numUnits = req.getParameter("num_vehicles");
+    String discount =req.getParameter("descompte");
+    if (!co2.isEmpty() && !engine.isEmpty() && !rentDays.isEmpty() && !numUnits.isEmpty() && !discount.isEmpty()) {
+	    if (co2.equals("54")) co2 = "Extralow";
+	    else if (co2.equals("71")) co2 = "Low";
+	    else if (co2.equals("82")) co2 = "Medium";
+	    else  if (co2.equals("139")) co2 = "High";
+	    out.println("<html><big>CO2 rating:"+ co2 +"</big><br></html>");
+	    out.println("<html><big>Engine:"+engine +"</big><br></html>");
+	    out.println("<html><big>Number of days:" +rentDays + "</big><br></html>");
+	    out.println("<html><big>Number of units:" + numUnits+ "</big><br></html>");
+	    out.println("<html><big>Discount(%):" + discount +  "</big><br></html>");
+	   
+           JSONObject obj = new JSONObject();
+          
+	   obj.put("CO2_rating", co2);
+	   obj.put("Engine", engine);
+	   obj.put("Number_of_days", rentDays);
+	   obj.put("Number_of_units", numUnits);
+	   obj.put("Discount", discount);
+	   list.add(obj);
+	   try (FileWriter file = new FileWriter("orders.json")) {
+		    file.write(list.toJSONString());
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	    }
+	   
+	  else out.println("Faltan campos por rellenar");
   }
 
   public void doPost(HttpServletRequest req, HttpServletResponse res)
